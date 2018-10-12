264da6d3eeeafbe9cef6b991b42d454439ba383f
CLN <cln@Sags-MacBook-Pro-2.local>
2018-10-12 14:04:20 -0400

    in process if figuring out ex1

--- hash-tables/ex1/ex1.py
+++ hash-tables/ex1/ex1.py
@@ -1,6 +1,34 @@
 def get_indices_of_item_weights(weights, limit):
-  pass 
+  i=0
+  
+  ht = {weights[i]:i}
+  
+  w = weights[i]
+  w2 = limit - weights[i]
+  while i < len(weights):
+    print(weights[i])
+    i += 1
+  return (weights[0],limit-weights[0])
+  # for w in range(len(weights)):
+  #   print(w)
+
 
 if __name__ == '__main__':
   # You can write code here to test your implementation using the Python repl
-  pass 
+  get_indices_of_item_weights([1,2,3,4,5,6],10) 
+
+# weights[i] + weights[j] = limits
+# w = weights[i]
+# w2 = limit - weights[i]
+ 
+# (w,w2) w>w2
+
+# colors = ["red", "green", "blue", "purple"]
+# for i in range(len(colors)):
+#     print(colors[i])
+
+# colors = ["red", "green", "blue", "purple"]
+# i = 0
+# while i < len(colors):
+#     print(colors[i])
+#     i += 1
